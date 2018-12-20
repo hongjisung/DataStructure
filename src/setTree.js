4edcfc9a473db9258e22a27540a0cc9325bdf299
jisung <rhfktj@gmail.com>
2018-12-20 16:12:35 +0900

    set setTree class and method

---
+++ src/setTree.js
@@ -0,0 +1,43 @@
+/*
+Red Black Tree
+
+class Node:
+  Method:
+    getData
+    setData
+    getParent
+    setParent
+    getLeftChild
+    setLeftChild
+    getRightChild
+    setRightChild
+
+class SetTree:
+  Constructor:
+    // parameter
+    data - null or iterable
+    compareFunction - inequality function of key
+
+  Method:
+    // iterator
+    begin
+    end
+    [Symbol.iterator]
+    // Capacity
+    empty
+    size
+    // Modifiers
+    clear
+    insert
+    merge
+    // Lookup
+    count
+    find
+    contains
+    equal_range
+    lower_bound
+    upper_bound
+    // Observers
+    key_comp
+
+*/
