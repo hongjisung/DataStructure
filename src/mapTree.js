36f43e6ef594f1a8a366905fff4561fc6f618429
jisung <rhfktj@gmail.com>
2018-12-28 19:04:24 +0900

    mapTree data structure
    it compare RedBlackTree.

---
+++ src/mapTree.js
@@ -0,0 +1,218 @@
+/*
+class MapTree:
+  Method:
+    // iterator
+    begin
+    end
+    rbegin
+    [Symbol.iterator]
+
+    // Capacity
+    empty
+    size
+
+    // Modifiers
+    clear
+    insert
+    erase
+
+    // Lookup
+    count
+    find
+    contains
+    equalRange
+    lowerBound
+    upperBound
+
+    // Observers
+    keyComp
+
+*/
+const RedBlackTree = require('./redBlackTree');
+
+/**
+ * @classdesc unique element self-balancing binary search tree.<br>
+ * key - value mapping.<br>
+ * Use composition by RedBlackTree.<br>
+ * Can not have mutiple same element.
+ */
+class MapTree {
+  /**
+   * Initiate MapTree.<br>
+   * Get parameter as null or compare function or other set tree.
+   * @param {null|function|MapTree} data
+   */
+  constructor(data = null) {
+    /**
+     * compose red black tree.
+     * @type {RedBlackTree}
+     * @private
+     */
+    this._tree = new RedBlackTree();
+    if (typeof data === 'function') {
+      this._tree = new RedBlackTree(data);
+    } else if (data instanceof MapTree) {
+      [...data].forEach(val => this._tree.insert(val[0], val[1]));
+    }
+  }
+
+  /**
+   * Return the first iterator node of tree.
+   * @returns {null|TreeNode}
+   */
+  begin() {
+    return this._tree.begin();
+  }
+
+  /**
+   * this express the end of iterating.<br>
+   * we can start from front with begin(), end with rbegin() and eventually meet end()<br>
+   * At the end, if we use getNext() method, we get false.<br>
+   * But, if we use getPrev() method, we get maximum node.
+   * @return {TreeNode} - endnode of tree iterator.
+   */
+  end() {
+    return this._tree.end();
+  }
+
+  /**
+   * return the last node of tree iterator.
+   * @returns {null|TreeNode}
+   */
+  rbegin() {
+    return this._tree.rbegin();
+  }
+
+  /**
+   * iterator of tree.<br>
+   * return value is [key, value] array.
+   */
+  [Symbol.iterator]() {
+    const itr = this._tree[Symbol.iterator]();
+    let data = null;
+    const iterator = {
+      next() {
+        data = itr.next();
+        if (data.done === false) {
+          return { value: data.value, done: false };
+        }
+        return { value: undefined, done: true };
+      },
+    };
+    return iterator;
+  }
+
+  /**
+   * make sure tree is empty.
+   * @returns {boolean}
+   */
+  emtpy() {
+    return this._tree.empty();
+  }
+
+  /**
+   * the number of nodes in tree.
+   * @returns {number}
+   */
+  size() {
+    return this._tree.size();
+  }
+
+  /**
+   * initiate the tree.
+   */
+  clear() {
+    this._tree.clear();
+  }
+
+  /**
+   * insert new node in tree with key.<br>
+   * If key already exists, return false.
+   * @param {*} key - Should not TreeNode.
+   */
+  insert(key, value) {
+    if (this._tree.contains(key)) {
+      return false;
+    }
+    this._tree.insert(key, value);
+    return true;
+  }
+
+  /**
+   * If erase() get key value as param, we erase all nodes with that key.<br>
+   * But if erase() get TreeNode as param, we erase only that node.
+   * @param {*|TreeNode} data - Data can be key or Node of tree.
+   * @returns {false|TreeNode} - next node of erased data. if cannot find the key, return falase.
+   */
+  erase(data) {
+    return this._tree.erase(data);
+  }
+
+  /**
+   * count the number of nodes which match the key.<br>
+   * It should be 0 or 1.
+   * @param {*} key - Should not TreeNode.
+   * @returns {number}
+   */
+  count(key) {
+    return this._tree.count(key);
+  }
+
+  /**
+   * return node which matches the key.
+   * @param {*} key - Should not TreeNode.
+   * @returns {TreeNode}
+   */
+  find(key) {
+    return this._tree.find(key);
+  }
+
+  /**
+   * make sure the key is in tree.
+   * @param {*} key - Should not TreeNode.
+   * @returns {boolean}
+   */
+  contains(key) {
+    return this._tree.contains(key);
+  }
+
+  /**
+   * return the start node and end node of given key.<br>
+   * end node is next node of last match node.<br>
+   * if no key in tree, return [endnode,endnode]
+   * @param {*} key - Should not TreeNode.
+   * @returns {Array} - [TreeNode,TreeNode]
+   */
+  equalRange(key) {
+    return this._tree.equalRange(key);
+  }
+
+  /**
+   * Find the first not less node.<br>
+   * It don't need to be same with given key.
+   * @param {*} key - the target key to find.
+   * @returns {TreeNode}
+   */
+  lowerBound(key) {
+    return this._tree.lowerBound(key);
+  }
+
+  /**
+   * Find the first upper node.<br>
+   * @param {*} key - the target key to find.
+   * @returns {TreeNode}
+   */
+  upperBound(key) {
+    return this._tree.upperBound(key);
+  }
+
+  /**
+   * return the key compare function.
+   * @returns {function}
+   */
+  keyComp() {
+    return this._tree.keyComp();
+  }
+}
+
+module.exports = MapTree;
---
+++ test/mapTreeTest.js
@@ -0,0 +1,154 @@
+const assert = require('assert');
+const MapTree = require('../src/mapTree');
+
+/* global describe it beforeEach */
+describe('Map Tree', () => {
+  let mapTree;
+
+  beforeEach('Initiate map tree', () => { mapTree = new MapTree(); });
+
+  it('constructor check', () => {
+    assert.strictEqual(mapTree.size(), 0);
+
+    mapTree = new MapTree((n1, n2) => n1 > n2);
+    mapTree.insert(3, 2);
+    mapTree.insert(5, 2);
+    mapTree.insert(4, 2);
+    mapTree.insert(2, 2);
+    mapTree.insert(1, 2);
+    assert.deepEqual([[1, 2], [2, 2], [3, 2], [4, 2], [5, 2]], [...mapTree]);
+
+    const st = new MapTree();
+    st.insert(3, 2);
+    st.insert(4, 2);
+    st.insert(5, 2);
+    st.insert(2, 2);
+    st.insert(1, 2);
+
+    mapTree = new MapTree(st);
+    assert.deepEqual([[5, 2], [4, 2], [3, 2], [2, 2], [1, 2]], [...mapTree]);
+  });
+
+  it('[Symbol.iterator] check', () => {
+    mapTree.insert(3, 7);
+    mapTree.insert(5, 2);
+    mapTree.insert(2, 8);
+    mapTree.insert(4, mapTree.end());
+    mapTree.insert(1, 'abc');
+    const iterator = mapTree[Symbol.iterator]();
+    let data = iterator.next();
+    assert.strictEqual(data.value[0], 5);
+    assert.strictEqual(data.value[1], 2);
+    data = iterator.next();
+    assert.strictEqual(data.value[0], 4);
+    assert.strictEqual(data.value[1], mapTree.end());
+    data = iterator.next();
+    assert.strictEqual(data.value[0], 3);
+    assert.strictEqual(data.value[1], 7);
+    data = iterator.next();
+    assert.strictEqual(data.value[0], 2);
+    assert.strictEqual(data.value[1], 8);
+    data = iterator.next();
+    assert.strictEqual(data.value[0], 1);
+    assert.strictEqual(data.value[1], 'abc');
+    data = iterator.next();
+    assert.strictEqual(data.value, undefined);
+  });
+
+  it('size() check', () => {
+    assert.strictEqual(mapTree.size(), 0);
+    mapTree.insert(3, 2);
+    mapTree.insert(4, 2);
+    mapTree.insert(5, 2);
+    assert.strictEqual(mapTree.size(), 3);
+    mapTree.insert(3, 2);
+    mapTree.insert(3, 2);
+    assert.strictEqual(mapTree.size(), 3);
+    mapTree.insert(4, 2);
+    assert.strictEqual(mapTree.size(), 3);
+    mapTree.insert(1, 2);
+    assert.strictEqual(mapTree.size(), 4);
+  });
+
+  it('insert() check', () => {
+    assert.strictEqual(mapTree.insert(3, 2), true);
+    assert.strictEqual(mapTree.insert(4, 2), true);
+    assert.strictEqual(mapTree.insert(1, 2), true);
+    assert.strictEqual(mapTree.insert(3, 2), false);
+    assert.strictEqual(mapTree.insert(1, 2), false);
+    assert.strictEqual(mapTree.insert(5, 2), true);
+  });
+
+  it('erase() check', () => {
+    assert.strictEqual(mapTree.erase(3), false);
+    mapTree.insert(3, 2);
+    mapTree.insert(3, 2);
+    mapTree.insert(3, 2);
+    assert.strictEqual(mapTree.erase(3), mapTree.end());
+    assert.strictEqual(mapTree.size(), 0);
+    assert.strictEqual(mapTree.erase(3), false);
+  });
+
+  it('count() check', () => {
+    assert.strictEqual(mapTree.count(3), 0);
+    mapTree.insert(3, 2);
+    mapTree.insert(3, 2);
+    assert.strictEqual(mapTree.count(3), 1);
+    mapTree.insert(4, 2);
+    mapTree.insert(6, 2);
+    assert.strictEqual(mapTree.count(6), 1);
+    mapTree.insert(1, 2);
+    mapTree.insert(6, 2);
+    assert.strictEqual(mapTree.count(1), 1);
+    assert.strictEqual(mapTree.count(2), 0);
+  });
+
+  it('find() check', () => {
+    assert.strictEqual(mapTree.find(3), mapTree.end());
+    mapTree.insert(3, 2);
+    mapTree.insert(3, 2);
+    mapTree.insert(4, 2);
+    assert.strictEqual(mapTree.find(4).getKey(), 4);
+    mapTree.insert(6, 2);
+    mapTree.insert(1, 2);
+    mapTree.insert(6, 2);
+    assert.strictEqual(mapTree.find(6).getKey(), 6);
+    assert.strictEqual(mapTree.find(3).getKey(), 3);
+  });
+
+  it('lowerBound() check', () => {
+    mapTree.insert(3, 2);
+    mapTree.insert(7, 2);
+    mapTree.insert(-3, 2);
+    mapTree.insert(4, 2);
+    mapTree.insert(1, 2);
+    let node = mapTree.lowerBound(5);
+    assert.strictEqual(node.getKey(), 4);
+    node = mapTree.lowerBound(3);
+    assert.strictEqual(node.getKey(), 3);
+    node = mapTree.lowerBound(8);
+    assert.strictEqual(node.getKey(), 7);
+    node = mapTree.lowerBound(-4);
+    assert.strictEqual(node.getColor(), null);
+    node = mapTree.lowerBound(1);
+    assert.strictEqual(node.getKey(), 1);
+  });
+
+  it('upperBound() check', () => {
+    mapTree.insert(3, 2);
+    mapTree.insert(7, 2);
+    mapTree.insert(-3, 2);
+    mapTree.insert(4, 2);
+    mapTree.insert(1, 2);
+    let node = mapTree.upperBound(5);
+    assert.strictEqual(node.getKey(), 4);
+    node = mapTree.upperBound(3);
+    assert.strictEqual(node.getKey(), 1);
+    node = mapTree.upperBound(8);
+    assert.strictEqual(node.getKey(), 7);
+    node = mapTree.upperBound(-4);
+    assert.strictEqual(node.getColor(), null);
+    node = mapTree.upperBound(1);
+    assert.strictEqual(node.getKey(), -3);
+  });
+});
