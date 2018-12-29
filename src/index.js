67212937d05e0fa8179d6d4006ac4d00f1d2002f
jisung <rhfktj@gmail.com>
2018-12-29 22:39:45 +0900

    setting index.js
    make index.js at ./src to export modules
    change package.json to distribution

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const Deque = require('./containers/deque');
+const List = require('./containers/list');
+const MapTree = require('./containers/mapTree');
+const PriorityQueue = require('./containers/priorityQueue');
+const Queue = require('./containers/queue');
+const SetTree = require('./containers/setTree');
+const Stack = require('./containers/stack');
+const QuickSort = require('./algorithms/quickSort');
+const MergeSort = require('./algorithms/mergeSort');
+
+module.exports = {
+  Deque,
+  List,
+  MapTree,
+  PriorityQueue,
+  Queue,
+  SetTree,
+  Stack,
+  QuickSort,
+  MergeSort,
+};
